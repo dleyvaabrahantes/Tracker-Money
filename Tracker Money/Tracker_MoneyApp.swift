7faa0546414548c64971600be10e605a3873ee3d
David Leyva <dleyvaabrahantes@gmail.com>
2022-05-26 09:34:39 -0400

    Initial Commit

---
+++ Tracker Money/Tracker_MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_MoneyApp.swift
+//  Tracker Money
+//
+//  Created by David on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
